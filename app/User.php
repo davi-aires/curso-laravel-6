37d8a3b87a7c2bea1163b82d53092c4d471638cc
davi-aires <daviaireslage@gmail.com>
2020-09-13 11:34:46 -0300

    Add Comments

--- app/User.php
+++ app/User.php
@@ -10,6 +10,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
