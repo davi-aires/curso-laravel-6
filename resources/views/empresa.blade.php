7ccc78d8d47e86a001200e4a85c352606bbabc13
davi-aires <daviaireslage@gmail.com>
2020-09-13 12:02:06 -0300

    Sobre rotas e URLs amigáveis

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,2 @@
+
+<h1>A empresa consiste em um curso</h1>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/contato', function () {
+    return view('site.contato');
+});
+
+Route::get('/empresa', function () {
+    return view('empresa');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
