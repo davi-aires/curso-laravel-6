8c4c35a1b6a6b7a91285edfa8497c1d1d74b9430
davi-aires <daviaireslage@gmail.com>
2020-09-13 13:01:09 -0300

    Rotas com parâmetros

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('produtos/{id?}', function($id = ''){
+    return "Produto(s) {$id}";
+});
+
+Route::get('/categoria/{flag}/comments', function ($flag){
+    return "Produtos da categoria: {$flag}</br>Você está nos comentários das categorias";
+});
+
+/*
+O nome da variavel só precisa ser condizente se houver algo específico dps da flag
+*/
+
+Route::get('/categoria/{flag}', function ($f){
+    return "Produtos da categoria: {$f}";
+});
+
 Route::any('/any', function(){
     return 'any';
 });
